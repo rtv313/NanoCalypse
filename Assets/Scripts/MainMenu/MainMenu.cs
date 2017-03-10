3f08485e6ef54d8a05370c0c957bd2ffcac62b43
Drixide77 <adria.abella.93@gmail.com>
2017-03-10 20:35:44 +0100

    Project now back to normal. Buttons updated to new API

---
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public int buttonIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+
+    public void buttonWasClicked()
+    {
+        if (buttonIndex == 0)
+        {
+            SceneManager.LoadScene("LevelStomach", LoadSceneMode.Single);
+        }
+        else if (buttonIndex == 1)
+        {
+            Debug.Log("About pressed.");
+        }
+        else if (buttonIndex == 2)
+        {
+            Application.Quit();
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
