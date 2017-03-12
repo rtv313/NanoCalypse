9ec178e16d83f18c9aed01cd6070cfbc9cac36c7
Raul Treviño <rtv031.itc@gmail.com>
2017-03-12 12:53:22 +0100

    Change Character for blue cubes and create manager for change static cameras

---
+++ Assets/Scripts/Camera/StaticCamerasManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticCamerasManager : MonoBehaviour {
+
+    public int index;
+    public List<Camera> cameras = new List<Camera>();
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        SelectCamera();
+    }
+
+    void activateCamera()
+    {
+        for (int i = 0; i < cameras.Count; i++)
+        {
+            cameras[i].enabled = false;
+
+            if (i == index)
+            {
+                cameras[i].enabled = true;
+            }
+            
+        }
+    }
+
+    void SelectCamera()
+    {
+        if (Input.GetKeyDown("p"))
+        {
+            index++;
+            if (index >= cameras.Count)
+                index = 0;
+            activateCamera();
+        }
+
+        if (Input.GetKeyDown("o"))
+        {
+            index--;
+            if (index < 0)
+                index = 0;
+            activateCamera();
+        }
+           
+     }
+}
