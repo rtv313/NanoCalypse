87a4f589f8b38f643e01c89e224921113d9ba2a3
AndresGregB <andresgregorioborrego@gmail.com>
2017-03-12 20:09:55 +0100

    Menus functionality finished

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -6,28 +6,46 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour {
 
     public int buttonIndex = 0;
-
+	private Canvas TitleCanvas, CanvasHelp, CanvasCredit;
 	// Use this for initialization
 	void Start () {
-        
+		CanvasHelp = GameObject.Find ("CanvasHelp").GetComponent<Canvas>();
+		TitleCanvas = GameObject.Find ("TitleCanvas").GetComponent<Canvas>();
+		CanvasCredit = GameObject.Find ("CanvasCredit").GetComponent<Canvas>();
+		CanvasHelp.enabled = false;
+		TitleCanvas.enabled = true;
+		CanvasCredit.enabled = false;
     }
 
     public void buttonWasClicked()
     {
-        if (buttonIndex == 0)
+        if (buttonIndex == 0)// New Game
         {
             SceneManager.LoadScene("LevelStomach", LoadSceneMode.Single);
         }
-        else if (buttonIndex == 1)
+        else if (buttonIndex == 1) // Help
         {
-            Debug.Log("About pressed.");
+			CanvasHelp.enabled = true;
+			TitleCanvas.enabled = false;
+			CanvasCredit.enabled = false;
+			Debug.Log ("Deactive");
         }
-        else if (buttonIndex == 2)
+        else if (buttonIndex == 2) // Credits
         {
-            Application.Quit();
+			CanvasHelp.enabled = false;
+			TitleCanvas.enabled = false;
+			CanvasCredit.enabled = true;
         }
+		else if (buttonIndex == 3) // Quit
+		{
+			Application.Quit();
+		}
     }
-
+	public void backToMain(){
+		CanvasHelp.enabled = false;
+		TitleCanvas.enabled = true;
+		CanvasCredit.enabled = false;
+	}
     // Update is called once per frame
     void Update () {
 
