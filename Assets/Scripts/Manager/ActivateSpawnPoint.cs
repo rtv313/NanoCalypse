a63845140e8f4ed08a2cb075bd077a1b7ca8d547
Raul Treviño <rtv031.itc@gmail.com>
2017-03-12 22:13:58 +0100

    Spawn Enemies with trigger

---
+++ Assets/Scripts/Manager/ActivateSpawnPoint.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateSpawnPoint : MonoBehaviour {
+
+    public GameObject spawnPoint;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player") {
+            spawnPoint.SetActive(true);
+            Destroy(transform.gameObject);
+        }
+    }
+}
