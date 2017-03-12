df6a95c145ce3d495419f9a945933528219dd1fb
Drixide77 <adria.abella.93@gmail.com>
2017-03-12 17:04:48 +0100

    Created Camera System (F1,F2,F3). Still missing a Pause feature.

---
+++ Assets/Scripts/Camera/CameraFreeMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFreeMovement : MonoBehaviour {
+
+    public float speed = 5.0f;
+    Vector3 movement;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        movement.Set(h, 0f, v);
+        movement = movement.normalized * speed * Time.deltaTime; // makes speed in diagonal the same
+        transform.position = transform.position + movement;
+    }
+}
