210376b065b52d665c869a5f1ab7934e403facf0
Drixide77 <adria.abella.93@gmail.com>
2017-03-14 20:47:04 +0100

    Added all the content from the delivery. Scene is a clusterfuck.

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -7,16 +7,44 @@ public class CameraFollow : MonoBehaviour
     public float smoothing = 5f;
 
     Vector3 offset;
+    private bool debug = false;
+    private Camera cam;
+
+    void OnPreRender()
+    {
+        if (debug)
+        {
+            GL.wireframe = true;
+                   }
+           }
+	void OnPostRender()
+    {
+        GL.wireframe = false;
+           }
 
     void Start()
     {
         offset = transform.position - target.position;
+        cam = this.gameObject.GetComponent<Camera>();
     }
 
     void FixedUpdate()
     {
         Vector3 targetCamPos = target.position + offset;
         transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.F11))
+        {
+            debug = !debug;
+            if (debug)
+            {
+                cam.clearFlags = CameraClearFlags.SolidColor;
+                           }
+            if (!debug)
+            {
+                cam.clearFlags = CameraClearFlags.Skybox;
+            }
+       }
     }
 	
 }
