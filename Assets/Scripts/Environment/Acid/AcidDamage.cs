71449db3013fd4d8a62ff8efbdb7fb8ff644103d
Raul Treviño <rtv031.itc@gmail.com>
2017-03-16 17:12:38 +0100

    Tides Scripts Done

---
+++ Assets/Scripts/Environment/Acid/AcidDamage.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AcidDamage : MonoBehaviour {
+
+    GameObject player;                         
+    PlayerHealth playerHealth;               
+    EnemyHealth enemyHealth;
+    public float timeBetweenAttacks = 0.5f;     // The time in seconds between each attack.
+    public int attackDamage = 10;
+    float timer;
+
+    void Awake()
+    {
+        // Setting up the references.
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.GetComponent<PlayerHealth>();
+        enemyHealth = GetComponent<EnemyHealth>();
+      
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            timer += Time.deltaTime;
+            if (timer >= timeBetweenAttacks)
+            {
+                // ... attack.
+                Attack();
+                timer = 0f;
+            }
+           
+        }
+    }
+
+
+    void Attack()
+    {
+        // Reset the timer.
+        timer = 0f;
+
+        // If the player has health to lose...
+        if (playerHealth.currentHealth > 0)
+        {
+            // ... damage the player.
+            playerHealth.TakeDamage(attackDamage);
+        }
+    }
+}
---
+++ Assets/Scripts/Environment/Acid/AcidTide.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AcidTide : MonoBehaviour {
+    public float speed = 1.0f;
+    public float height = 10f;
+    public bool flagGoUp = false;
+
+    private Vector3 from;
+    private Vector3 to;
+    private float percentage=0.0f;
+    private Vector3 result;
+  
+    // Use this for initialization
+    void Start () {
+        from = transform.position;
+        to = transform.position;
+        to.y += height;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (flagGoUp == true)
+        {
+            tideUp();
+        }
+        else
+        {
+            tideDown();
+        }
+    }
+
+    void tideUp()
+    {
+        percentage +=  speed * Time.deltaTime;
+        if (percentage >= 1.0f)
+            percentage = 1.0f;
+        result = Vector3.Lerp(from, to, percentage);
+        transform.position = result;
+    }
+
+    void tideDown()
+    {
+        percentage -= speed * Time.deltaTime;
+        if (percentage <= 0.0f)
+         percentage = 0.0f;
+        result = Vector3.Lerp(from, to, percentage);
+        transform.position = result;
+    }
+}
