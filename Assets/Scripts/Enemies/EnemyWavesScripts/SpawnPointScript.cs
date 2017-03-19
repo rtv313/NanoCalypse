58de0bd248f0520dac91d1937cc3c910f6c71fcc
Raul Treviño <rtv031.itc@gmail.com>
2017-03-19 16:42:13 +0100

    Waves System Done

---
+++ Assets/Scripts/Enemies/EnemyWavesScripts/SpawnPointScript.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPointScript : MonoBehaviour {
+
+    public int timeToSpawnMin;
+    public int timeToSpawnMax;
+    public int timeToSpawnEnemyInEffect=1;
+    public int coolDownTime = 5;
+    public bool flagCreate = true;
+    public GameObject spawnEnemyEffect;
+    public GameObject[] enemies;
+    public enum  EnemyType { VIRUS, BACTERIA, PARASITE, ALL };
+    public EnemyType  enemyType = EnemyType.ALL;
+   
+    private int enemySelector=0;
+    private GameObject spawnEnemyEffectForDestroy;
+
+    void Update()
+    {
+        if (flagCreate)
+        {
+            createEnemyPrevious();
+        }
+    }
+
+    void createEnemyPrevious()
+    {
+        flagCreate = false;
+        int randomTime = Random.Range(timeToSpawnMin, timeToSpawnMax);
+        Invoke("enemyCreationEffect", randomTime);
+    }
+
+    void enemyCreationEffect()
+    {
+        spawnEnemyEffectForDestroy = Instantiate(spawnEnemyEffect, transform.position, transform.rotation); 
+
+        if (enemyType == EnemyType.ALL)
+        {
+            enemySelector = Random.Range(0, enemies.Length);
+            Invoke("createEnemy", timeToSpawnEnemyInEffect);
+            return;
+        }
+
+        if (enemyType == EnemyType.VIRUS)
+        {
+            enemySelector = 0;
+            Invoke("createEnemy", timeToSpawnEnemyInEffect);
+            return;
+        }
+
+        if (enemyType == EnemyType.BACTERIA)
+        {
+            enemySelector = 1;
+            Invoke("createEnemy", timeToSpawnEnemyInEffect);
+            return;
+        }
+
+        if (enemyType == EnemyType.PARASITE)
+        {
+            enemySelector = 2;
+            Invoke("createEnemy", timeToSpawnEnemyInEffect);
+            return;
+        }
+
+    }
+
+    void createEnemy()
+    {
+        GameObject enemy = enemies[enemySelector];
+        Instantiate(enemy, transform.position, transform.rotation);
+        Invoke("coolDown", coolDownTime);
+    }
+
+    void createCertainEnemy(EnemyType setEnemyType)
+    {
+        enemyType = setEnemyType;
+    }
+
+    void destroySpawnPoint()
+    {
+        Destroy(gameObject);
+    }
+
+    void coolDown()
+    {
+        flagCreate = true;
+        Destroy(spawnEnemyEffectForDestroy);
+    }
+}
---
+++ Assets/Scripts/Enemies/EnemyWavesScripts/WaveManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SpawnPointListWrapper
+{
+    public string name;
+    public  List<GameObject> spawnPoints;
+}
+
+public class WaveManager : MonoBehaviour {
+    public bool activate = true;
+    public List<SpawnPointListWrapper> wavesSpawnPointsLists;
+
+    public void activateLists(List<int> activateWavesList)
+    {
+        foreach (int x in activateWavesList)
+        {
+            SpawnPointListWrapper listSpawnPoints = wavesSpawnPointsLists[x];
+
+            foreach(GameObject i in listSpawnPoints.spawnPoints)
+            {
+                i.SetActive(true);
+            }
+        }
+    }
+
+    public void deactivateLists(List<int> deactivateFaseList)
+    {
+        foreach (int x in deactivateFaseList)
+        {
+            SpawnPointListWrapper listSpawnPoints = wavesSpawnPointsLists[x];
+
+            foreach (GameObject i in listSpawnPoints.spawnPoints)
+            {
+                i.SetActive(false);
+            }
+        }
+    }
+
+    public void destroyWavesSystem()
+    {
+        foreach (SpawnPointListWrapper spawnPointList in wavesSpawnPointsLists)
+        {
+            foreach (GameObject spawnPoint in spawnPointList.spawnPoints)
+            {
+                Destroy(spawnPoint);
+            }
+        }
+    }
+
+    void Update() //for testing only
+    {
+        List<int> waves = new List<int>();
+        waves.Add(0);
+        waves.Add(1);
+
+        if (activate) {
+             activateLists(waves);
+        } else{
+            deactivateLists(waves);  
+        }
+    }
+}
