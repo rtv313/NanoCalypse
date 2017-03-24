52d99d25f46e3311b12d48845603571c540e1eae
Raul Treviño <rtv031.itc@gmail.com>
2017-03-24 16:26:02 +0100

    ShootingSystem Finished without effects

--- Assets/Scripts/Player/ShootingSystem/projectile.cs
+++ Assets/Scripts/Player/ShootingSystem/projectile.cs
@@ -8,12 +8,17 @@ public class projectile : MonoBehaviour {
     public GameObject sniperBullet;
     public GameObject shootgunBullet;
     public Transform bulletSpawn;
-    public int bulletSpeed = 10;
+    public int bulletSpeedRifle=35;
+    public int bulletSpeedShootgun = 35;
+    public int bulletSpeedSniper = 35;
     public int fireMode=1;
     public float timer=1;
     public float timeBetweenBulletsAssaultRifle = 0.0f;
     public float timeBetweenBulletsShootgun = 0.0f;
     public float timeBetweenBulletSniperRifle = 0.0f;
+    public float rifleBulletLifeTime = 2.0f;
+    public float shootgunBulletLifeTime = 0.5f;
+    public float sniperBulletLifeTime = 3.0f;
 
     void Update()
     {
@@ -53,20 +58,21 @@ public class projectile : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             fireMode = 1;
-            timer = 0.0f;
+            timer = 1.0f;
 
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             fireMode = 2;
-            timer = 0.0f;
+            timer = 1.0f;
         }
 
 
-        if (Input.GetKeyDown(KeyCode.Alpha3)){
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
             fireMode = 3;
-            timer = 0.0f;
+            timer = 1.0f;
         }
     }
 
@@ -75,46 +81,66 @@ public class projectile : MonoBehaviour {
         // Create the Bullet from the Bullet Prefab
         if (fireMode == 1)
         {
-            var bullet = (GameObject)Instantiate(assaultRifleBullet, bulletSpawn.position, bulletSpawn.rotation);
-            // Add velocity to the bullet
-            bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletSpeed;
-            timer = 0.0f;
-            // Destroy the bullet after 2 seconds
-            Destroy(bullet, 2.0f);
-
+            shootAssaultRifle();
         }
         else if (fireMode == 2)
         {
-            Quaternion rotationQuaternion;
-            rotationQuaternion = bulletSpawn.rotation;
-            var bullet = (GameObject)Instantiate(shootgunBullet, bulletSpawn.position, bulletSpawn.rotation);
-            bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletSpeed;
-            Destroy(bullet, 2.0f);
-
-            rotationQuaternion.y = 0;
-            rotationQuaternion.y += 30;
-            bullet = (GameObject)Instantiate(shootgunBullet, bulletSpawn.position, bulletSpawn.rotation);
-            bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletSpeed;
-            Destroy(bullet, 2.0f);
-
-            rotationQuaternion.y = 0;
-            rotationQuaternion.y -= 30;
-
-            bullet = (GameObject)Instantiate(shootgunBullet, bulletSpawn.position, bulletSpawn.rotation);
-            bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletSpeed;
-            Destroy(bullet, 2.0f);
-
-            timer = 0.0f;
+            shootShootgun();
         }
         else if (fireMode == 3){
-             
-            var bullet = (GameObject)Instantiate(sniperBullet, bulletSpawn.position, bulletSpawn.rotation);
-            // Add velocity to the bullet
-            bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletSpeed;
-            timer = 0.0f;
-            // Destroy the bullet after 2 seconds
-            Destroy(bullet, 2.0f);
+            shootSniperRifle();
         }
        
+    }
+
+    void shootAssaultRifle()
+    {
+        var bullet = (GameObject)Instantiate(assaultRifleBullet, bulletSpawn.position, bulletSpawn.rotation);
+        // Add velocity to the bullet
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletSpeedRifle;
+        timer = 0.0f;
+        // Destroy the bullet after 2 seconds
+        Destroy(bullet, rifleBulletLifeTime);
+    }
+
+    void shootShootgun()
+    {
+        var bullet = (GameObject)Instantiate(shootgunBullet, bulletSpawn.position, bulletSpawn.rotation);
+        bullet.transform.Rotate(bullet.transform.rotation.x, 5, bullet.transform.rotation.z);
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletSpeedShootgun;
+        Destroy(bullet, shootgunBulletLifeTime);
+
+        bullet = (GameObject)Instantiate(shootgunBullet, bulletSpawn.position, bulletSpawn.rotation);
+        bullet.transform.Rotate(bullet.transform.rotation.x, 10, bullet.transform.rotation.z);
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletSpeedShootgun;
+        Destroy(bullet, shootgunBulletLifeTime);
+
+        bullet = (GameObject)Instantiate(shootgunBullet, bulletSpawn.position, bulletSpawn.rotation);
+        bullet.transform.Rotate(bullet.transform.rotation.x, -5, bullet.transform.rotation.z);
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletSpeedShootgun;
+        Destroy(bullet, shootgunBulletLifeTime);
+
+        bullet = (GameObject)Instantiate(shootgunBullet, bulletSpawn.position, bulletSpawn.rotation);
+        bullet.transform.Rotate(bullet.transform.rotation.x, -10, bullet.transform.rotation.z);
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletSpeedShootgun;
+        Destroy(bullet, shootgunBulletLifeTime);
+
+        bullet = (GameObject)Instantiate(shootgunBullet, bulletSpawn.position, bulletSpawn.rotation);
+        bullet.transform.Rotate(bullet.transform.rotation.x, bullet.transform.rotation.y, bullet.transform.rotation.z);
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletSpeedShootgun;
+        Destroy(bullet, shootgunBulletLifeTime);
+
+        timer = 0.0f;
+    }
+
+
+    void shootSniperRifle()
+    {
+        var bullet = (GameObject)Instantiate(sniperBullet, bulletSpawn.position, bulletSpawn.rotation);
+        // Add velocity to the bullet
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletSpeedSniper;
+        timer = 0.0f;
+        // Destroy the bullet after 2 seconds
+        Destroy(bullet, sniperBulletLifeTime);
     }
 }
