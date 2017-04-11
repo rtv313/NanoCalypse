86d0f3f37bc6c0502038d692d020aa0d719b725e
Raul Treviño <rtv031.itc@gmail.com>
2017-04-11 10:33:21 +0200

    Bullet damage to enemy done

---
+++ Assets/Scripts/Player/ShootingSystem/BulletDamage.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class BulletDamage : MonoBehaviour {
+
+    public enum BulletType { ASSAULT, SHOOTGUN,SNIPER };
+    public BulletType bulletType = BulletType.ASSAULT;
+    public int assaultRifleDamage = 10;
+    public int shootgunDamage = 5;
+    public int sniperDamage = 30;
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            Context context = other.gameObject.GetComponent<Context>();
+
+            switch (bulletType)
+            {
+                case BulletType.ASSAULT:
+                    context.life -= assaultRifleDamage / 2;
+                    break;
+
+                case BulletType.SHOOTGUN:
+                    context.life -= shootgunDamage / 2;
+                    break;
+
+                case BulletType.SNIPER:
+                    context.life -= sniperDamage / 2;
+                    break;
+            }
+
+            Destroy(transform.gameObject);
+        }
+    }
+}
