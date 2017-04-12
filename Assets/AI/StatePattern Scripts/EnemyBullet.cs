0f1d9c2edef41011711b91fd829986ef16e964ba
Raul Treviño <rtv031.itc@gmail.com>
2017-04-12 17:26:17 +0200

    Enemy Bullet damage done

---
+++ Assets/AI/StatePattern Scripts/EnemyBullet.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour {
+
+    public int damage = 5;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            PlayerHealth pH = other.gameObject.GetComponent<PlayerHealth>();
+            pH.TakeDamage(damage);
+            Destroy(transform.gameObject);
+        }
+
+        if (other.gameObject.tag != "Enemy" && other.gameObject.tag != "AgentDetector")
+        {
+            Destroy(transform.gameObject);
+        }
+
+    }
+}
