330906bca1ed5b0f2df14e601335141998f9f86f
Raul Treviño <rtv031.itc@gmail.com>
2017-04-14 18:22:28 +0200

    basic drones spawn Player

--- Assets/AI/DroneAI/CreateDrones.cs
+++ Assets/AI/DroneAI/CreateDrones.cs
@@ -4,13 +4,53 @@ using UnityEngine;
 
 public class CreateDrones : MonoBehaviour {
 
+    public GameObject Drone;
+
+    public GameObject spawnPointOne;
+    public GameObject spawnPointTwo;
+    public GameObject spawnPointThree;
+
+
+    public GameObject wound;
+    public GameObject healthPointOne;
+    public GameObject healthPointTwo;
+    public GameObject healthPointThree;
+
+    public bool spawnedDrones = false;
+    public bool canDeployDrones = false;
+
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update() {
+        SpawnDrones();
+
+    }
+
+    void SpawnDrones()
+    {
+
+        if (Input.GetKeyDown(KeyCode.F) && spawnedDrones== false)
+        {
+            GameObject drone = Instantiate(Drone, spawnPointOne.transform.position, spawnPointOne.transform.rotation);
+            drone.GetComponent<DroneContext>().wound = wound;
+            drone.GetComponent<DroneContext>().woundPosition = healthPointOne;
+            drone.GetComponent<DroneContext>().player = transform.gameObject;
+
+            drone = Instantiate(Drone, spawnPointTwo.transform.position, spawnPointOne.transform.rotation);
+            drone.GetComponent<DroneContext>().wound = wound;
+            drone.GetComponent<DroneContext>().woundPosition = healthPointTwo;
+            drone.GetComponent<DroneContext>().player = transform.gameObject;
+
+            drone = Instantiate(Drone, spawnPointThree.transform.position, spawnPointOne.transform.rotation);
+            drone.GetComponent<DroneContext>().wound = wound;
+            drone.GetComponent<DroneContext>().woundPosition = healthPointThree;
+            drone.GetComponent<DroneContext>().player = transform.gameObject;
+
+            spawnedDrones = true;
+        }
+    }
 }
