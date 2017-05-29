a783aa9bf3379d44dc63d363b9cd6ef37f6c37f9
AndresGregB <andresgregorioborrego@gmail.com>
2017-05-29 21:35:23 +0200

    particles for shoreline done

---
+++ Assets/quicktest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quicktest : MonoBehaviour {
+	Transform tran;
+	// Use this for initialization
+	void Start () {
+		tran = GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		tran.Translate (0.0f, Mathf.Sin (Time.time)/10, 0.0f);
+	}
+}
