b4e1f22fd1e4fb743865e3a464cf7f9bfd1d398d
AndresGregB <andresgregorioborrego@gmail.com>
2017-06-22 22:01:11 +0200

    Ambientation refined

---
+++ Assets/Scripts/remove_fog.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class remove_fog : MonoBehaviour {
+
+	private bool revertFogState = false;
+	void OnPostRender() {
+		revertFogState = RenderSettings.fog;
+		RenderSettings.fog = enabled;
+	}
+	void OnPreRender() {
+		RenderSettings.fog = revertFogState;
+	}
+}
