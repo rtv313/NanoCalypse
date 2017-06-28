8b558d99f7515d69cdc31a594181f354c9993606
Drixide77 <adria.abella.93@gmail.com>
2017-06-28 23:14:42 +0200

    Gameplay completed

---
+++ Assets/AmbushTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbushTrigger : MonoBehaviour {
+
+    public GameObject[] enemies;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Ambush Activated!");
+            for (int i = 0; i < enemies.Length; ++i)
+            {
+                enemies[i].SetActive(true);
+            }
+            Destroy(transform.gameObject);
+        }
+    }
+}
