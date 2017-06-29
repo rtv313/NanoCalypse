57e772cd9a7e68f4462fad8ba91c73cd6bc956bc
Raul Treviño <rtv031.itc@gmail.com>
2017-06-29 16:00:10 +0200

    Acid changes in level 1

---
+++ Assets/BarrierKill.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrierKill : MonoBehaviour {
+    public int numSpawnsLinked;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+        if (numSpawnsLinked <= 0) {
+            Debug.Log("DESTROYING ");
+            Destroy(this.gameObject);
+        }
+	}
+}
---
+++ Assets/spawnMessageBarrier.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnMessageBarrier : MonoBehaviour {
+    public BarrierKill barrier;
+     
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnDisable() {
+        --barrier.numSpawnsLinked;
+    }
+}
