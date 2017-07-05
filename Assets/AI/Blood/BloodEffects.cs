40f955ce1fbb9ec3199acbc796bbbe271ef6a3d5
Raul Treviño <rtv031.itc@gmail.com>
2017-07-05 12:52:05 +0200

    blood spawn position fixed

---
+++ Assets/AI/Blood/BloodEffects.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodEffects : MonoBehaviour
+{
+    public GameObject bloodPrefab;
+    public float heightPosition=1.0f;
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Bullet")
+        {
+            Vector3 spawnPosition = transform.position;
+            spawnPosition.y += heightPosition;
+            Quaternion rotateSpawnPosition = other.transform.rotation;
+            rotateSpawnPosition.y = 180;
+            Instantiate(bloodPrefab, spawnPosition, rotateSpawnPosition);
+        }
+    }
+
+}
