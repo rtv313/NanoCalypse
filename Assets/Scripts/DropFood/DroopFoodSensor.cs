4b875ba2d7b4d847d6bc1a21609fb828c640de30
Raul Treviño <rtv031.itc@gmail.com>
2017-09-05 18:03:43 +0200

    Droop Food Scenario halfway done

---
+++ Assets/Scripts/DropFood/DroopFoodSensor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroopFoodSensor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<DropFood>().enabled = false;
+    }
+	
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag =="Player")
+            gameObject.GetComponent<DropFood>().enabled = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+            gameObject.GetComponent<DropFood>().enabled = false;
+    }
+
+}
