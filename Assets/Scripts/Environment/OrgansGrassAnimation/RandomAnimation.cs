b8b0a9da30c9bae2d1c98bef588b1af0ae8f0184
Raul Treviño <rtv031.itc@gmail.com>
2017-09-07 22:31:48 +0200

    Random animations in plants and organs done

---
+++ Assets/Scripts/Environment/OrgansGrassAnimation/RandomAnimation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomAnimation : MonoBehaviour {
+
+    public int [] randomTimes;
+    private Animator animatorControl;
+    private bool canCallAnimation = true;
+   
+    // Use this for initialization
+    void Start ()
+    {
+        animatorControl = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (canCallAnimation == true)
+        {
+            canCallAnimation = false;
+            Invoke("CallAnimation", randomTimes[Random.Range(0, randomTimes.Length)]);
+        }
+	}
+
+    void CallAnimation()
+    {
+        animatorControl.SetTrigger("activateAnimation");
+       
+    }
+
+    public void AnimationFinished()
+    {
+        canCallAnimation = true;
+    }
+}
