b99ff4294c1c80aded0425b74d39488499985b80
Raul Treviño <rtv031.itc@gmail.com>
2017-09-14 14:06:13 +0200

    Change Attack distande based on color with one bug in melee

--- Assets/Scripts/AI Boss/SelectBossAttack.cs
+++ Assets/Scripts/AI Boss/SelectBossAttack.cs
@@ -7,6 +7,11 @@ public class SelectBossAttack : MonoBehaviour {
     public GameObject meshBody;
     public float brightness = 1.6f;
     public float changeTime = 10.0f;
+
+    public float laserAttackDistance = 15.0f;
+    public float airAttackDistance = 18.0f;
+    public float meleeAttackDistance = 7.0f;
+
     private bool  flagChangeAttack = false;
     private BossContext context;
     private float controlTime =0.0f;
@@ -37,24 +42,26 @@ public class SelectBossAttack : MonoBehaviour {
 
     private void SelectAttackType()
     {
-        //int random = Random.Range(1,5);
-        int random = 2;
+        int random = Random.Range(1,5);
         Material mat = meshBody.GetComponent<Renderer>().material;
 
         switch (random)
         {
             case 1:
                 context.BossColor = Context.EnemyType.VIRUS;
+                context.attackDistance = laserAttackDistance;
                 mat.SetColor("_EmissionColor", Color.red * brightness);
                 break;
 
             case 2:
                 context.BossColor = Context.EnemyType.PARASITE;
+                context.attackDistance = meleeAttackDistance;
                 mat.SetColor("_EmissionColor", Color.blue * brightness);
                 break;
 
             case 3:
                 context.BossColor = Context.EnemyType.BACTERIA;
+                context.attackDistance = airAttackDistance;
                 mat.SetColor("_EmissionColor", Color.green * brightness);
                 break;
          }
