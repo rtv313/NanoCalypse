383407442900df336e47cfc70e7241f0f00e443f
AndresGregB <andresgregorioborrego@gmail.com>
2017-09-25 20:19:52 +0200

    Game UI almost done

---
+++ Assets/Scripts/button_effects.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button_effects : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void buttonOver ()
+	{
+		gameObject.GetComponent<RectTransform> ().sizeDelta = new Vector2 (160f, 28f);
+
+	}
+	public void buttonOut(){
+		gameObject.GetComponent<RectTransform> ().sizeDelta = new Vector2 (160f, 30f);
+	}
+}
