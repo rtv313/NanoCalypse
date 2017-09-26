c3de527e030dca6dc35043643aee1fe8e1c4fed5
AndresGregB <andresgregorioborrego@gmail.com>
2017-09-27 00:31:42 +0200

    UI for mines done

---
+++ Assets/Scripts/mine_ui_counter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class mine_ui_counter : MonoBehaviour {
+	private PlayerShockWave mines;
+	public Image[] mine_images;
+	private int num_mines;
+	// Use this for initialization
+	void Start () {
+		mines = GameObject.Find ("Player").GetComponent<PlayerShockWave> ();
+		num_mines = mines.minesLimit - mines.mineCounter;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		num_mines = mines.minesLimit - mines.mineCounter;
+		for (int i = 0; i < 5; i++) {
+			mine_images [i].color = new Color (0.4f,0.4f,0.4f,0.4f);
+		}
+		for (int i = 0; i < num_mines; i++) {
+			mine_images [i].color = new Color (1.0f,1.0f,1.0f,1.0f);
+		}
+	}
+}
